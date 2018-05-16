fc81336b4fd69b15155c8cb1c78d7c7107a9d1a7
Peter Roome <pete@lostmy.name>
2018-05-16 22:09:27 +0100

    Rake task

---
+++ lib/tasks/sms.rb
@@ -0,0 +1,66 @@
+desc 'set of console tasks for ops team'
+namespace :sms do
+  desc 'Send an SMS to all guests to save the date'
+  task :save_the_date => [:environment] do |t, args|
+    session = GoogleDrive.saved_session(
+      "./config/google_config_new.json",
+      nil,
+      ENV["GOOGLE_CLIENT_ID"],
+      ENV["GOOGLE_CLIENT_SECRET"]
+    )
+
+    ws = session.spreadsheet_by_key("1-DVANo0uHCxvv8CBpXY-DsbKvQhSbS9_jaUkmlASfEE").worksheets[6]
+
+    guests = []
+    (2..3).each do |row|
+      person = {
+        number: ws[row, 1],
+        name: ws[row, 2]
+      }
+      p "#{person[:name]} - #{person[:number]}"
+      guests << person
+    end
+
+    @twilio = Twilio::REST::Client.new(ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"])
+    guests.each_with_index do |guest, index|
+      row = (index + 1) + 1 # Headings are on row 1
+
+      message = "❤️⭐️❤️⭐️❤️⭐️❤️⭐️
+
+      Dear #{guest[:name]},
+
+      💌 Save the date!
+
+      Jen & Pete are delighted to invite you to their wedding.
+
+      September 15th, 2018.
+
+      The Copse, Mill Lane, Kidmore End, Oxon, RG4 9HA.
+      Map 🗺 -> https://goo.gl/vZdmsp
+
+      The ceremony will begin at 2pm. More details to follow.
+
+      Please text YES if you are saving the date and can join us. Text NO if sadly, you have something better to do 😉.
+
+      ❤️⭐️❤️⭐️❤️⭐️❤️⭐️"
+
+      begin
+        @twilio.api.account.messages.create(
+          from: '+441915803991',
+          to: '+447834510945',
+          body: message
+        )
+
+        ws[row, 3] = ws[row, 3].blank? ? 1 : ws[row, 3].to_i + 1
+        ws.save
+        ws.reload
+      rescue => e
+        Rails.logger.error(
+          "name=#{guest[:name]}" +
+          "number=#{guest[:number]}" +
+          "error=#{e.message}"
+        )
+      end
+    end
+  end
+end
--- scratch_pad.rb
+++ scratch_pad.rb
@@ -1,6 +1,3 @@
-require "google_drive"
-require 'twilio-ruby'
-
 session = GoogleDrive.saved_session(
   "./config/google_config_new.json",
   nil,
@@ -14,39 +11,40 @@ guests = []
 
 (2..3).each do |row|
   person = {
-    number: ws[row, 1]
-    name: ws[row, 2],
+    number: ws[row, 1],
+    name: ws[row, 2]
   }
   p "#{person[:name]} - #{person[:number]}"
   guests << person
 end
 
 @twilio = Twilio::REST::Client.new(ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"])
-message = "❤️⭐️❤️⭐️❤️⭐️❤️⭐️
-
-💌 Save the date!
-
-Jen & Pete are delighted to invite you to their wedding
-
-September 15th, 2018.
-
-The Copse, Mill Lane, Kidmore End, Oxon, RG4 9HA.
-Map 🗺 -> https://goo.gl/vZdmsp
-
-The ceremony will begin at 2pm.
-
-More details about accommodation and food will follow shortly!
-
-Please text YES if you are saving the date and can join us. Text NO if sadly, you won't be able to be with us.
-
-❤️⭐️❤️⭐️❤️⭐️❤️⭐️"
 
 guests.each_with_index do |guest, index|
   row = (index + 1) + 1 # Headings are on row 1
 
+  message = "❤️⭐️❤️⭐️❤️⭐️❤️⭐️
+
+  Dear #{guest[:name]},
+
+  💌 Save the date!
+
+  Jen & Pete are delighted to invite you to their wedding.
+
+  September 15th, 2018.
+
+  The Copse, Mill Lane, Kidmore End, Oxon, RG4 9HA.
+  Map 🗺 -> https://goo.gl/vZdmsp
+
+  The ceremony will begin at 2pm. More details to follow.
+
+  Please text YES if you are saving the date and can join us. Text NO if sadly, you have something better to do 😉.
+
+  ❤️⭐️❤️⭐️❤️⭐️❤️⭐️"
+
   @twilio.api.account.messages.create(
     from: '+441915803991',
-    to: guest[:number],
+    to: '+447834510945',
     body: message
   )
 
